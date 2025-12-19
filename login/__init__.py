510742be2515bf99b26b4b6ab11e4549e6f2beaf
Shammas-tvr <muhammadshammastvr@gmail.com>
2025-12-19 22:44:45 +0530

    css problem solved / new login desing UI

--- login/__init__.py
+++ login/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 
 login_bp = Blueprint('login', __name__,
-                        template_folder='templates',
-                        static_folder='static'
+                     template_folder='templates',
+                     static_folder='static',
+                     static_url_path='/login/static'  # Add this line
                     )
 
 from . import routes
