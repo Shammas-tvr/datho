cb14daa0729b81a1f6bbf83a00fdf9aa496972aa
Shammas-tvr <muhammadshammastvr@gmail.com>
2025-12-20 21:51:09 +0530

    project changed to django from flask

--- login/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-login_bp = Blueprint('login', __name__,
-                     template_folder='templates',
-                     static_folder='static',
-                     static_url_path='/login/static'  # Add this line
-                    )
-
-from . import routes
